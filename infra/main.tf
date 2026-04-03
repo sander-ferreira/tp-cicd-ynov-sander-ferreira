23cb247a64a2fbf94953f5ef5cb12272336f8137
Yoann Auroy <yoann.ar.pro@gmail.com>
2026-04-03 12:02:23 +0200

    feat: create terraform job

--- infra/main.tf
+++ infra/main.tf
@@ -1,84 +1,84 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.92"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
     }
-    required_version = ">= 1.2"
+  }
+  required_version = ">= 1.2"
 }
 
 provider "aws" {
-    region = "eu-west-3" #Paris
+  region = "eu-west-3" #Paris
 }
 
 # 1. AMI Ubuntu 24.04
-data "aws_ami" "ubuntu"{
-    most_recent = true
-    owners = ["099720109477"]
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
 
-    filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
 }
 
 # 2. Création de la clé SSH
 resource "tls_private_key" "pk" {
   algorithm = "RSA"
-  rsa_bits = 4096
+  rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "generated_key" {
-  key_name = "registry-key-terraform"
+  key_name   = "registry-key-terraform"
   public_key = tls_private_key.pk.public_key_openssh
 }
 
 # Sauvegarde de la clé privée locale Ansible
 resource "local_file" "ssh_key" {
-    filename = "${path.module}/registry-key-terraform.pem"
-    content = tls_private_key.pk.private_key_pem
-    file_permission = "0400"
+  filename        = "${path.module}/registry-key-terraform.pem"
+  content         = tls_private_key.pk.private_key_pem
+  file_permission = "0400"
 }
 
 resource "aws_security_group" "registry_sg" {
-    name = "registry-sg-simple" 
-    description = "Allow SSH, HTTPS and HTTP"
-    ingress {
-        description = "SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        description = "HTTP port for registry"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  name        = "registry-sg-simple"
+  description = "Allow SSH, HTTPS and HTTP"
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP port for registry"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        description = "HTTPS port for registry"
-        from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "HTTPS port for registry"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # 4. Instance EC2
 resource "aws_instance" "registry_server" {
-  ami = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name = aws_key_pair.generated_key.key_name
+  key_name      = aws_key_pair.generated_key.key_name
 
   vpc_security_group_ids = [aws_security_group.registry_sg.id]
 
@@ -98,6 +98,6 @@ output "instance_ip" {
 }
 
 output "private_key_pem" {
-    value     = tls_private_key.pk.private_key_pem
-    sensitive = true
+  value     = tls_private_key.pk.private_key_pem
+  sensitive = true
 }

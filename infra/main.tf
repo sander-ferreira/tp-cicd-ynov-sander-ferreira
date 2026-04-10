efcbc6113731b2d7fef777f1f0350a29bd507b50
Sander <sander@MacBook-Pro-de-Sander.local>
2026-04-10 14:44:53 +0200

    infra(ec2): bump instance type to t3.small for deploy stability

--- infra/main.tf
+++ infra/main.tf
@@ -77,7 +77,7 @@ resource "aws_security_group" "app_sg" {
 # 4. Instance EC2
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   key_name      = aws_key_pair.generated_key.key_name
 
   vpc_security_group_ids = [aws_security_group.app_sg.id]
